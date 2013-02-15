myusuf3/numbers.vim

commit bbead6b5f4e141883e97bf4c24f8d674908ff1d4

Add more commands and configuration.
The effects of the plugin can now be activated and deactivated.

plugin/numbers.vim
@@ -74,19 +74,43 @@ function! Uncenter()
     call ResetNumbers()
 endfunc
 
-" Triggers mode based on events
-augroup NumbersAug
-    au!
-    autocmd InsertEnter * :call SetNumbers()
-    autocmd InsertLeave * :call SetRelative()
-    autocmd BufNewFile  * :call ResetNumbers()
-    autocmd BufReadPost * :call ResetNumbers()
-    autocmd FocusLost   * :call Uncenter()
-    autocmd FocusGained * :call Center()
-augroup END
+function! NumbersEnable()
+    let g:enable_numbers = 1
+    augroup NumbersAug
+        au!
+        autocmd InsertEnter * :call SetNumbers()
+        autocmd InsertLeave * :call SetRelative()
+        autocmd BufNewFile  * :call ResetNumbers()
+        autocmd BufReadPost * :call ResetNumbers()
+        autocmd FocusLost   * :call Uncenter()
+        autocmd FocusGained * :call Center()
+    augroup END
+endfunc
+
+function! NumbersDisable()
+    let g:enable_numbers = 0
+    augroup NumbersAug
+        au!
+    augroup END
+endfunc
+
+function! NumbersOnOff()
+    if (g:enable_numbers == 1)
+        call NumbersDisable()
+    else
+        call NumbersEnable()
+    endif
+endfunc
 
 " Commands
 command! -nargs=0 NumbersToggle call NumbersToggle()
+command! -nargs=0 NumbersEnable call NumbersEnable()
+command! -nargs=0 NumbersDisable call NumbersDisable()
+command! -nargs=0 NumbersOnOff call NumbersOnOff()
 
 " reset &cpo back to users setting
 let &cpo = s:save_cpo
+
+if (!exists('g:enable_numbers') || g:enable_numbers)
+	call NumbersEnable()
+endif

commit 19574c266be6fcd18b272fa1ea800a55c1f6c95d

Fixes an issue introduced in bbead6b5f.
There was an issue where NumbersOnOff was not always working. I think
this fixes that problem.

plugin/numbers.vim
@@ -25,6 +25,10 @@ if exists("g:loaded_numbers") && g:loaded_numbers
 endif
 let g:loaded_numbers = 1
 
+if (!exists('g:enable_numbers'))
+	let g:enable_numbers = 1
+endif
+
 if v:version < 703 || &cp
     echomsg "numbers.vim: you need at least Vim 7.3 and 'nocp' set"
     echomsg "Failed loading numbers.vim"
@@ -111,6 +115,6 @@ command! -nargs=0 NumbersOnOff call NumbersOnOff()
 " reset &cpo back to users setting
 let &cpo = s:save_cpo
 
-if (!exists('g:enable_numbers') || g:enable_numbers)
+if (g:enable_numbers)
 	call NumbersEnable()
 endif